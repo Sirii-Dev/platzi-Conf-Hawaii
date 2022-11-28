d57a4724457cf83a1d77cfe2942fee9351f44429
Sarahi Esquivel Pardo <sarahiesquivel45@gmail.com>
2022-11-27 20:45:32 -0600

    maquetacion pagina principal

---
+++ index.js
@@ -0,0 +1,3 @@
+//Activando todos los tooltips
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
